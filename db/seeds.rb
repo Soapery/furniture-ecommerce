ac57ac0ce189cbbcb1f677bd5010fded96307b88
Soapery <darian.a.ronald@gmail.com>
2023-12-03 22:58:37 -0600

    Added code to seed provinces and products tables.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,126 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "csv"
+
+# Clearing existing records for re-seeding
+User.delete_all
+Province.delete_all
+Product.delete_all
+Order.delete_all
+OrderProduct.delete_all
+
+# Resetting PK on tables
+ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name='users';")
+ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name='provinces';")
+ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name='products';")
+ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name='orders';")
+ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name='order_products';")
+
+# Retrieving CSV Data
+filename = Rails.root.join("db/housewares.csv")
+puts "Loading Products from the CSV file: #{filename}"
+
+# Province data
+provinces = [
+  {
+    :name => "Alberta",
+    :gst =>  0.05,
+    :pst => nil,
+    :hst => nil
+  },
+  {
+    :name => "British Columbia",
+    :gst => 0.05,
+    :pst => 0.07,
+    :hst => nil
+  },
+  {
+    :name => "Manitoba",
+    :gst => 0.05,
+    :pst => 0.07,
+    :hst => nil
+  },
+  {
+    :name => "New Brunswick",
+    :gst => 0.05,
+    :pst => nil,
+    :hst => nil
+  },
+  {
+    :name => "Newfoundland and Labrador",
+    :gst => nil,
+    :pst => nil,
+    :hst => 0.15
+  },
+  {
+    :name => "Northwest Territories",
+    :gst => 0.05,
+    :pst => nil,
+    :hst => nil
+  },
+  {
+    :name => "Nova Scotia",
+    :gst => nil,
+    :pst => nil,
+    :hst => 0.15
+  },
+  {
+    :name => "Nunavut",
+    :gst => 0.05,
+    :pst => nil,
+    :hst => nil
+  },
+  {
+    :name => "Ontario",
+    :gst => nil,
+    :pst => nil,
+    :hst => 0.13
+  },
+  {
+    :name => "Prince Edward Island",
+    :gst => nil,
+    :pst => nil,
+    :hst => 0.15
+  },
+  {
+    :name => "Quebec",
+    :gst => 0.05,
+    :pst => 0.09975,
+    :hst => nil
+  },
+  {
+    :name => "Saskatchewan",
+    :gst => 0.05,
+    :pst => 0.06,
+    :hst => nil
+  },
+  {
+    :name => "Yukon",
+    :gst => 0.05,
+    :pst => nil,
+    :hst => nil
+  },
+]
+
+provinces.map{ |province|
+  puts province
+  Province.create(
+    name: province[:name],
+    gst:  province[:gst],
+    pst:  province[:pst],
+    hst:  province[:hst]
+  )
+}
+
+# Populating Products table
+# Encoding in UTF-8 while removing BOM
+CSV.foreach(filename, 'r:BOM|UTF-8', headers: true) do |row|
+  # Replaces "NA" values with equivalent values
+  product = Product.create(
+    name:      row["Name"],
+    variation: row["Variation"] == "NA" ? nil : row["Variation"],
+    pattern:   row["Pattern"] == "NA" ? nil : row["Pattern"],
+    outdoor:   row["Outdoor"] == "Yes" ? true : false,
+    price:     Faker::Commerce.price,
+    on_sale:   0
+  )
+  puts product.inspect
+end

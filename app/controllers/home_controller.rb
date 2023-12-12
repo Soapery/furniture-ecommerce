a7b0756114c0b7c3006a4399597995b2546bcc6d
Soapery <darian.a.ronald@gmail.com>
2023-12-12 15:36:04 -0600

    Added pagination and links to product pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
-    @products = Product.all
+    if params[:search]
+      @products = Product.where("name LIKE ?", "%#{params[:search]}%").paginate(page: params[:page], per_page: 40)
+    else
+      @products = Product.paginate(page: params[:page], per_page: 40)
+    end
   end
 end

3e531f3f33a7010d0947751e2ee8456d1a270e64
Soapery <darian.a.ronald@gmail.com>
2023-12-13 14:48:09 -0600

    Adjusted migrations

--- db/migrate/20231201222316_create_products.rb
+++ db/migrate/20231201222316_create_products.rb
@@ -1,12 +1,11 @@
 class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
-
       t.string :name, limit: 50
       t.boolean :outdoor
       t.decimal :price
       t.decimal :on_sale
-      t.string  :description,  limit: 600
+      t.string  :description, limit: 600
 
       t.timestamps
     end

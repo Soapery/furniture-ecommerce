c933aa91832f70b11896ff6d3d669921b915a145
Soapery <darian.a.ronald@gmail.com>
2023-12-12 22:39:34 -0600

    Now generates buttons

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,8 @@
 class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
+
+    @variations = @product.product_variations
+    @patterns = @product.product_patterns
   end
 end

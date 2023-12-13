02648e5143854e8af0f1764550d161ed3e09e319
reidpan1 <reidpan3@gmail.com>
2023-12-12 22:32:00 -0600

    reseed and remove variations run

--- db/migrate/20231212232805_remove_variation_and_pattern_from_products.rb
+++
@@ -1,6 +0,0 @@
-class RemoveVariationAndPatternFromProducts < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :products, :variation, :string
-    remove_column :products, :pattern, :string
-  end
-end

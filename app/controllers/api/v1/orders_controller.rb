7da0150457d732857116b51ef3762b7b9d64b13c
Soapery <darian.a.ronald@gmail.com>
2023-12-15 14:11:43 -0600

    Fixed nesting error

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -14,65 +14,65 @@ module Api
           payment_method: "Credit Card"
         )
 
-        if @order.save
-          Rails.logger.debug @order.inspect
-          Order.transaction do
-            session[:cart].map do |product_id, quantity|
-              product = Product.find(product_id)
-              order_product = OrderProduct.new(
-                order:      @order,
-                product:,
-                amount:     quantity,
-                base_cost:  product.price,
-                gst:        @province.gst,
-                pst:        @province.pst,
-                hst:        @province.hst,
-                total_cost: product.price * (@province.pst.to_f + @province.gst.to_f + @province.hst.to_f + 1)
-              )
-              order_product.save
-            end
-            session[:cart] = {}
-          end
-        end
+        return unless @order.save
 
-        def checkout
-          @user = User.find(current_user.id)
-          Rails.logger.debug(current_user.inspect)
-          @province = Province.find(current_user.province_id)
-
-          @cart_products = session[:cart].map do |product_id, quantity|
+        Rails.logger.debug @order.inspect
+        Order.transaction do
+          session[:cart].map do |product_id, quantity|
             product = Product.find(product_id)
-            { product:, quantity: }
+            order_product = OrderProduct.new(
+              order:      @order,
+              product:,
+              amount:     quantity,
+              base_cost:  product.price,
+              gst:        @province.gst,
+              pst:        @province.pst,
+              hst:        @province.hst,
+              total_cost: product.price * (@province.pst.to_f + @province.gst.to_f + @province.hst.to_f + 1)
+            )
+            order_product.save
           end
+          session[:cart] = {}
+        end
+      end
 
-          @subtotal = calculate_subtotal(@cart_products)
-          @total = calculate_total(@subtotal, @province.gst,
-                                   @province.pst, @province.hst)
-          session[:order_total] = @total
+      def checkout
+        @user = User.find(current_user.id)
+        Rails.logger.debug(current_user.inspect)
+        @province = Province.find(current_user.province_id)
 
-          render "checkout/show"
+        @cart_products = session[:cart].map do |product_id, quantity|
+          product = Product.find(product_id)
+          { product:, quantity: }
         end
 
-        def calculate_subtotal(cart_products)
-          cart_products.sum { |item| item[:product].price * item[:quantity] }
-        end
+        @subtotal = calculate_subtotal(@cart_products)
+        @total = calculate_total(@subtotal, @province.gst,
+                                 @province.pst, @province.hst)
+        session[:order_total] = @total
 
-        def calculate_total(subtotal, pst, gst, hst)
-          subtotal * (pst.to_f + gst.to_f + hst.to_f + 1)
-        end
+        render "checkout/show"
+      end
 
-        def check_shipping_info(user)
-          return true unless user.postal_code.blank? || current_user.address.blank? || current_user.province_id.blank?
+      def calculate_subtotal(cart_products)
+        cart_products.sum { |item| item[:product].price * item[:quantity] }
+      end
 
-          flash[:alert] = "Please provide shipping address information to confirm the order."
-          redirect_to checkout_checkout_path
-          false
-        end
+      def calculate_total(subtotal, pst, gst, hst)
+        subtotal * (pst.to_f + gst.to_f + hst.to_f + 1)
+      end
 
-        def order_params
-          params.permit(:user_id, :base_cost, :total_cost, :credit_card_number, :credit_card_exp,
-                        :credit_card_cvv, :authenticity_token, :commit)
-        end
+      def check_shipping_info(user)
+        return true unless user.postal_code.blank? || current_user.address.blank? || current_user.province_id.blank?
+
+        flash[:alert] = "Please provide shipping address information to confirm the order."
+        redirect_to checkout_checkout_path
+        false
+      end
+
+      def order_params
+        params.permit(:user_id, :base_cost, :total_cost, :credit_card_number, :credit_card_exp,
+                      :credit_card_cvv, :authenticity_token, :commit)
       end
     end
   end
